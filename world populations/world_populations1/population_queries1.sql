2e811b6e3aec4a6a0989347a0a78bb7a3b610056
srinivasulu07 <56213536+srinivasulu07@users.noreply.github.com>
2020-11-29 13:06:16 +0530

    Update population_queries1.sql

--- world populations/world_populations1/population_queries1.sql
+++ world populations/world_populations1/population_queries1.sql
@@ -1,10 +1,17 @@
-/***select * from population_years;
+/***select * from population_years;***/
+--what are the distinct years in this dataset?
 select distinct year from population_years;
+--What is the largest population size for Gabon in this dataset?
 select max(population) from population_years
 where country="Gabon";
+--What were the 10 lowest population countries in 2005?
 SELECT *  from population_years
 where year=2005
 order by population asc
-limit 10;***/
-select * from population_years
-where country="Indonesia" and (year=2000 or year=2010);
+limit 10;
+--What are all the distinct countries with a population of over 100 million in the year 2010?
+SELECT country FROM population_years
+WHERE population > 100 AND year = 2010;
+--How many countries in this dataset have the word “Islands” in their name?
+SELECT DISTINCT country FROM population_years
+WHERE country LIKE '%Islands%';
